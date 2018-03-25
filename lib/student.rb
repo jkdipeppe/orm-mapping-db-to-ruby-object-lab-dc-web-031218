e88dfabd869ec80fbcfff9b9cb713afafe1f1f78
github username <github email address>
2018-03-25 13:09:42 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,29 +1,54 @@
+require 'pry'
+
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    new_student = Student.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+
+    all_students = DB[:conn].execute(sql)
+    student_instances = []
+
+    all_students.map do |row|
+      student_instances << Student.new_from_db(row)
+    end
+
+    student_instances
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE students.name = (?)
+    SQL
+    new_student = DB[:conn].execute(sql, name)
+    # binding.pry
+    Student.new_from_db(new_student[0])
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
@@ -40,4 +65,32 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.count_all_students_in_grade_9
+    all_students = Student.all
+    all_students.select {|student| student.grade == '9'}
+  end
+
+
+  def self.students_below_12th_grade
+    all_students = Student.all
+    all_students.select {|student| student.grade != '12'}
+  end
+
+  def self.first_student_in_grade_10
+    all_students = Student.all
+    all_students.find {|student| student.grade == '10'}
+  end
+
+  def self.all_students_in_grade_X(grade)
+    all_students = Student.all
+    all_students.select {|student| student.grade == grade.to_s}
+  end
+
+  def self.first_X_students_in_grade_10(number)
+    all_students = Student.all
+    grade_10_students = all_students.select {|student| student.grade == '10'}
+    grade_10_students.slice(0, number)
+  end
+
 end
